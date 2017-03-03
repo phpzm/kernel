2f099eda9117f67ba35ee0eace08248338a1b412
William Correa <wilcorrea@gmail.com>
2017-03-02 23:03:12 -0300

    Add Wrapper class

---
+++ src/Kernel/Wrapper.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Simples\Kernel;
+
+/**
+ * Class Wrapper
+ * @package Simples\Route
+ */
+abstract class Wrapper
+{
+    /**
+     * @var array
+     */
+    private static $messages = [];
+
+    /**
+     * @param $message
+     */
+    public static function warning($message)
+    {
+        self::message('warning', $message);
+    }
+
+    /**
+     * @param $message
+     */
+    public static function info($message)
+    {
+        self::message('info', $message);
+    }
+
+    /**
+     * @param $message
+     */
+    public static function buffer($message)
+    {
+        self::message('buffer', $message);
+    }
+
+    /**
+     * @param $log
+     */
+    public static function log($log)
+    {
+        self::message('log', $log);
+    }
+
+    /**
+     * @param $type
+     * @param $message
+     */
+    public static function message($type, $message)
+    {
+        self::$messages[] = [
+            'type' => $type,
+            'message' => $message,
+            'trace' => self::trace()
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public static function messages()
+    {
+        return self::$messages;
+    }
+
+    /**
+     * @return array
+     */
+    protected static function trace()
+    {
+        $stack = App::beautifulTrace(debug_backtrace());
+
+        return array_slice($stack, 3);
+    }
+}
