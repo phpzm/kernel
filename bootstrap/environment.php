2d0505b8ed566a5c3eafb095fa5fc9a0e46f71e1
William Correa <wilcorrea@gmail.com>
2017-03-31 00:44:35 -0300

    Environment variables - add new constants to project (AND, OR, COMMA)

--- bootstrap/environment.php
+++ bootstrap/environment.php
@@ -5,9 +5,9 @@ error_reporting(E_ALL);
 
 // system settings
 ini_set('display_errors', env('ERRORS_DISPLAY', 'On'));
-ini_set('log_errors', env('ERRORS_DISPLAY', 'On'));
-ini_set('track_errors', env('ERRORS_DISPLAY', 'Off'));
-ini_set('html_errors', env('ERRORS_DISPLAY', 'Off'));
+ini_set('log_errors', env('ERRORS_LOG', 'On'));
+ini_set('track_errors', env('ERRORS_TRACK', 'Off'));
+ini_set('html_errors', env('ERRORS_HTML', 'Off'));
 
 // native types
 const TYPE_BOOLEAN = 'boolean';
@@ -23,8 +23,9 @@ const TYPE_UNKNOWN_TYPE = 'unknown type';
 // custom types
 const TYPE_DATE = 'date';
 
-const __AND__ = 'AND';
-const __OR__ = 'OR';
+const __AND__ = ' AND ';
+const __OR__ = ' OR ';
+const __COMMA__ = ', ';
 
 // used to compose path do generator
 define('TEMPLATE_DIR', 'kernel/resources/templates');
@@ -39,6 +40,7 @@ if (!function_exists('error_handler')) {
      */
     function error_handler($code, $message, $file, $line)
     {
+
         throw new ErrorException($message, $code, 1, $file, $line);
     }
     set_error_handler("error_handler");
